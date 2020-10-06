2694e92a9725f9c44324f84f62257f9b31ea571b
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-10-06 23:55:57 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,7 @@ docker-compose -f ${file} up -d
 docker-compose logs -f
 ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
 
-echo ""
+echo "Portainer - http://$ipAddress:9000"
 echo "Jenkins — http://$ipAddress:8080"
 echo "SonarQube — http://$ipAddress:9000"
-echo "GitLab — http://$ipAddress:10080"
-echo "Gerrit — http://$ipAddress:8070"
-echo "Portainer - http://$ipAddress:9000"
-echo "Jenkins Initial Password - "
-cat /var/lib/docker/volumes/ddi_cbcd_jenkins-volume/_data/secrets/initialAdminPassword
-
-  
-  
-  
-  
-  
+echo "Nexus — http://$ipAddress:8081"
