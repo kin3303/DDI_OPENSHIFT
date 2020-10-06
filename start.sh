e65e3cb364450cdf211645fa4fcf7cd368304985
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-10-07 00:06:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,5 +8,5 @@ ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
 
 echo "Portainer - http://$ipAddress:9000"
 echo "Jenkins — http://$ipAddress:8080"
-echo "SonarQube — http://$ipAddress:9000"
+echo "SonarQube — http://$ipAddress:8090"
 echo "Nexus — http://$ipAddress:8081"
