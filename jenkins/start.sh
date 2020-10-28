fc8fe311016fbf80918bcb12dba689d604832a73
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-10-29 02:19:31 +0900

    Update start.sh

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -4,7 +4,7 @@ file=${1:-docker-compose.yaml}
 
 docker-compose -f ${file} up -d
 docker-compose logs -f
-ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
+ipAddress="$(curl ifconfig.me)"
 
 echo "Portainer - http://$ipAddress:9000"
 echo "Jenkins — http://$ipAddress:8080"
