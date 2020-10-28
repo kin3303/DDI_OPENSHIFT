504c1cb91dcbf60aea58b10593689f1b7d3d8e0b
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-10-29 02:11:33 +0900

    Update start-nexus.sh

--- start-nexus.sh
+++ start-nexus.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-docker-compose -f docker-compose-nexus.yaml up -d
-docker-compose logs -f
-ipAddress="$(dig @resolver1.opendns.com ANY myip.opendns.com +short)"
-
-echo "Portainer - http://$ipAddress:9000"
-echo "Nexus — https://$ipAddress"
+docker-compose up -d
+docker-compose logs -f 
