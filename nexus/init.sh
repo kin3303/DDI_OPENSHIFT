dc33d7a2de68f71d55ff4a8591855710259ca70a
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-10-30 09:48:07 +0900

    Update init.sh

--- nexus/init.sh
+++ nexus/init.sh
@@ -3,7 +3,7 @@
 CUR_DIR="$(dirname "$0")"
 source "${CUR_DIR}/.env"
 
-NEXUS_VOL_DIR="${CUR_DIR}/nexus-data"
+NEXUS_VOL_DIR="${VOL_BASE_DIR}/nexus"
 mkdir -p "${NEXUS_VOL_DIR}"
 chown 200:200 "${NEXUS_VOL_DIR}"
 
